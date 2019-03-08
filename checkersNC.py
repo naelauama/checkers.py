1cc36a179f2b3624592c5e088af137dd0db691b0
csPython <cspython@AD25TF04NGF1J-42.local>
2019-03-08 09:28:59 -0500

    grid created

--- checkersNC.py
+++ checkersNC.py
@@ -10,4 +10,9 @@ sqSz = 50
 chWin =GraphWin("Checkers", sqSz * 10, sqSz * 10)
 chWin.setCoords(0, 0, sqSz * 10, sqSz * 10)
 
+for i in range(8):
+    square = Rectangle(Point(sqSz(i * 1, sqSz(i * 1)), Point(i * 2, sqSz*2))
+    square.setFill(color_rgb(240,0,0))
+    square.draw(chWin)
+
 draw_sq(sqSz, sqSz, sqSz, "red", chWin)
