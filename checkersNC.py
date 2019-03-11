13f0966f5b309e1d27b5d2ce1ef490829ade9b08
csPython <cspython@AD25TF04NGF1J-42.local>
2019-03-11 10:51:51 -0400

    grid added & working

--- checkersNC.py
+++ checkersNC.py
@@ -1,18 +1,18 @@
 from graphics import *
 
 def draw_sq(sX, sY, size, color, win):
-    square = Rectangle(Point(sX, sY), Point(sX * 2, sY * 2))
-    square.setFill(color_rgb(255,0,0))
+    square = Rectangle(Point(sX, sY), Point(sX + size, sY + size))
+    square.setFill(color)
     square.draw(win)
 
 sqSz = 50
+sqCol = "red"
 
 chWin =GraphWin("Checkers", sqSz * 10, sqSz * 10)
 chWin.setCoords(0, 0, sqSz * 10, sqSz * 10)
 
-for i in range(8):
-    square = Rectangle(Point(sqSz(i * 1, sqSz(i * 1)), Point(i * 2, sqSz*2))
-    square.setFill(color_rgb(240,0,0))
-    square.draw(chWin)
+for j in range(8):
+    for i in range(8):
+        draw_sq(sqSz * (i + 1), sqSz *(j+1), sqSz, sqCol, chWin)
 
-draw_sq(sqSz, sqSz, sqSz, "red", chWin)
+        
